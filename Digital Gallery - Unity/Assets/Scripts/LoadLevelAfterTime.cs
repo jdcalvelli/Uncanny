a30bf7070d67e51e46a4cc08f330778297b5c2a5
JD Calvelli <john_calvelli@brown.edu>
2020-04-28 14:25:07 -0400

    add intro and outro scenes, scene change script

---
+++ Digital Gallery - Unity/Assets/Scripts/LoadLevelAfterTime.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadLevelAfterTime : MonoBehaviour
+{
+    [SerializeField] //this is so that you can see private variables in the unity editor
+    private float delayBeforeLoading = 10f;
+    [SerializeField]
+    private string sceneNameToLoad;
+    
+    private float timeElapsed;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        timeElapsed += Time.deltaTime;
+
+        if(timeElapsed > delayBeforeLoading)
+        {
+            SceneManager.LoadScene(sceneNameToLoad);
+        }
+    }
+}
